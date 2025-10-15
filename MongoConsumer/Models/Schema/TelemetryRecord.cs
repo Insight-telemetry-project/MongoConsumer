06b52dc2c2c15bf45b370bc2f1c4009fb3a6a0b1
nadav5 <nadavbar205@gmail.com>
2025-10-15 14:21:30 +0300

    [IMPROVE] Work with mongo schema

---
+++ MongoConsumer/Models/Schema/TelemetryRecord.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+public class TelemetryRecord
+{
+    [BsonElement("Fields")]
+    public Dictionary<string, double> Fields { get; set; } = new();
+
+    [BsonElement("timestep")]
+    public int Timestep { get; set; }
+
+    [BsonElement("Master Index")]
+    public int MasterIndex { get; set; }
+}
